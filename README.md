bc0e536f7a43c9aca6a72a269177b74ffe49661d
Karvendhan <karvendhaneie@gmail.com>
2020-04-18 03:21:13 +0530

    go

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PythonWorks
-Journey towards DataEngineer
+Journey towards DataEngineer!
 This is where i planned to learn the python for data analysis
--- myfirstfile.py
+++ myfirstfile.py
@@ -3,3 +3,4 @@ print("Hello World")
 # this is my comment on learning python
 # this is my commit test on github with visual studio
 # Allset with the setup
+
