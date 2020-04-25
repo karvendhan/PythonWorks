3c15e3ab66eed777d2ce4dca1bad5fd1d26b1b9f
Karvendhan <karvendhaneie@gmail.com>
2020-04-25 12:07:05 +0530

    t

--- CoolApps/MouseMover.py
+++ CoolApps/MouseMover.py
@@ -1,9 +1,14 @@
-import win32api
+import win32api 
 import time
 import math
-for i in range(500):
+
+time.sleep(20)
+
+for i in range(5):
     x=int(500+math.sin(math.pi*i/100)*500)
     y=int(500+math.cos(i)*100)
     win32api.SetCursorPos((x,y))
     time.sleep(1)
+
+print("hello world")
 
